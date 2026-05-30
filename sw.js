0eddcc0331ddfbbd9be381ecd0725eb73651f702
dhiraj khapangi <dkhapangi13@outlook.com>
2026-05-30 14:26:13 +0545

    resolve crashing of brave issue

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cricket-scoreboard-v1";
+const CACHE_NAME = "cricket-scoreboard-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./app-logo.png"];
 
 // Install Service Worker and cache all vital assets
